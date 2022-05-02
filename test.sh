6c8258cd42ccda926a58cc437757b93099e99fb3
kirill kuksa <kirilltd@tut.by>
2022-05-02 17:45:01 +0300

    action

--- test.sh
+++ test.sh
@@ -1,8 +1,13 @@
 #! /bin/bash
 
-file_list=`git diff HEAD HEAD~1 --name-only | grep -v "workflow"`
-
-if [ -z "$file_list" ]
-then 
-  echo "file list is empty" 
+file_list=`git diff HEAD HEAD~2 --name-only | grep -v "workflow"`
+if [ -z $file_list ]
+then
+    echo "Changed files not found"
+else
+    echo "1"
+    sudo apt-get install jq
+    json_file_list=$(echo $file_list | jq -R -s -c 'split("\n")[:-1]')
+    echo $json_file_list
+    echo "::set-output name=files::$json_file_list"
 fi
